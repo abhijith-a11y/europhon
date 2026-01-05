cdd5fc49a02092f50f695a5bc899586dfa6a18a3
abhijith-a11y <abhijith@lement8.ae>
2026-01-05 14:14:25 +0530

    home slider 01

--- template-parts/home_sections/home_slider_new.php
+++ template-parts/home_sections/home_slider_new.php
@@ -1,74 +1,97 @@
- <section class="home_slider_new">
- <div class="home_slider_new_col_01">
-    <div class="swiper slider-col-01">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <div class="slide-content">
-                    
-
-             
-                <img src="<?php echo get_template_directory_uri(); ?>/images/slide_01.png" alt="Home Slider 01" />
+<section class="home_slider_new">
+    <div class="home_slider_new_wrapper">
+        <div class="home_slider_new_col_01">
+            <div class="swiper slider-col-01">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="slide-content">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/slide_01.png"
+                                alt="Home Slider 01" />
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="slide-content">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/slide_02.png"
+                                alt="Home Slider 01" />
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="slide-content">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/slide_01.png"
+                                alt="Home Slider 01" />
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="slide-content">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/slide_02.png" alt="Home Slider 01" />
-                </div>
+                <!-- Navigation arrows -->
+                <!-- <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div> -->
             </div>
         </div>
-        <!-- Navigation arrows -->
-        <!-- <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div> -->
-    </div>
- </div>
 
- <div class="home_slider_new_col_02">
-    <div class="swiper slider-col-02">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <div class="slide-content">
-                    <h3 class="f_28">Euro Systems Private Villa Acoustic Ceilings</h3>
-                    <!-- <h4>sleek modern transparency</h4> -->
-                    <!-- <p>Symfony® acoustic glass partitions systems are designed for transparency without compromise. Symfony® acoustic glass partitions bring clarity, openness, and acoustic control into one seamless system.</p>      -->
+        <div class="home_slider_new_col_02">
+            <div class="swiper slider-col-02">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="slide-content">
+                            <h3 class="f_28">Euro Systems Private Villa Acoustic Ceilings</h3>
+                            <!-- <h4>sleek modern transparency</h4> -->
+                            <!-- <p>Symfony® acoustic glass partitions systems are designed for transparency without compromise. Symfony® acoustic glass partitions bring clarity, openness, and acoustic control into one seamless system.</p>      -->
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="slide-content">
+                            <h3 class="f_28">Euro Systems Healthcare Sidra Medical Center</h3>
+                            <!-- <h4>sleek modern transparency</h4> -->
+                            <!-- <p>Symfony® acoustic glass partitions systems are designed for transparency without compromise. Symfony® acoustic glass partitions bring clarity, openness, and acoustic control into one seamless system.</p>      -->
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="slide-content">
+                            <h3 class="f_28">Euro Systems Private Villa Acoustic Ceilings</h3>
+                            <!-- <h4>sleek modern transparency</h4> -->
+                            <!-- <p>Symfony® acoustic glass partitions systems are designed for transparency without compromise. Symfony® acoustic glass partitions bring clarity, openness, and acoustic control into one seamless system.</p>      -->
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="swiper-slide">
-            <div class="slide-content">
-                    <h3 class="f_28">Euro Systems Healthcare Sidra Medical Center</h3>
-                    <!-- <h4>sleek modern transparency</h4> -->
-                    <!-- <p>Symfony® acoustic glass partitions systems are designed for transparency without compromise. Symfony® acoustic glass partitions bring clarity, openness, and acoustic control into one seamless system.</p>      -->
-                </div>
+                <!-- Navigation arrows -->
+                <!-- <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div> -->
             </div>
         </div>
-        <!-- Navigation arrows -->
-        <!-- <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div> -->
-    </div>
- </div>
 
- <div class="home_slider_new_col_03">
-    <div class="swiper slider-col-03">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <div class="slide-content">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/slide_01_child.png" alt="Home Slider 01" />
+        <div class="home_slider_new_col_03">
+            <div class="swiper slider-col-03">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="slide-content">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/slide_02_child.png"
+                                alt="Home Slider 01" />
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="slide-content">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/slide_01_child.png"
+                                alt="Home Slider 01" />
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="slide-content">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/slide_02_child.png"
+                                alt="Home Slider 01" />
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="slide-content">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/slide_02_child.png" alt="Home Slider 01" />
-                </div>
+                <!-- Navigation arrows -->
+                <!-- <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div> -->
             </div>
         </div>
-        <!-- Navigation arrows -->
-        <!-- <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div> -->
-    </div>
- </div>
- 
- <!-- Single Shared Pagination -->
 
- <div class="shared-pagination-wrapper">
- <div class="shared-pagination"></div>
- </div>
- </section>
+        <!-- Single Shared Pagination -->
+
+        <div class="shared-pagination-wrapper">
+            <div class="shared-pagination"></div>
+        </div>
+    </div>
+</section>
