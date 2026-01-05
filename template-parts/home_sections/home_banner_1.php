849916e030a722ab72621271f0d31850e34676ab
abhijith-a11y <abhijith@lement8.ae>
2026-01-05 11:06:13 +0530

    home banner slider complete

--- template-parts/home_sections/home_banner_1.php
+++ template-parts/home_sections/home_banner_1.php
@@ -1,30 +1,59 @@
-
 <section class="waper_banner_main">
- 
-<div class="banner-main-slider-item" style="background:red;">
 
-
-<div class="container" style="position: relative; z-index: 3;">
-                    <div class="banner_content text_center">
-                        <div class="tag_line color_white f_24" data-aos="fade-up" data-aos-duration="1200"
-                            data-aos-once="true">Design. Engineering. Acoustic Excellence.</div>
-                        <h1 class="f_80 color_white" data-aos="fade-up" data-aos-duration="1400" data-aos-once="true">
-                            VOICE OF THE MAKER</h1>
-                        <div data-aos="fade-up" data-aos-duration="1600" data-aos-once="true">
-                            <a href="/contact-us" class="btn_style">Enquire Now</a>
-                        </div>
-                    </div>
+    <div class="banner-main-slider-item">
+        <div class="container" style="position: relative; z-index: 3;">
+            <div class="banner_content text_center">
+                <div class="tag_line color_white f_24" data-aos="fade-up" data-aos-duration="1200" data-aos-once="true">
+                    Design. Engineering. Acoustic Excellence.</div>
+                <h1 class="f_80 color_white mt_30" data-aos="fade-up" data-aos-duration="1400" data-aos-once="true">
+                    VOICE OF THE MAKER</h1>
+                <div data-aos="fade-up" data-aos-duration="1600" data-aos-once="true" class="mt_40">
+                    <a href="/contact-us" class="btn_style">Enquire Now</a>
                 </div>
+            </div>
+        </div>
+        <div
+            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: radial-gradient(50% 50% at 50% 50%, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.6) 100%); z-index: 2;">
+        </div>
+        <video autoplay loop muted playsinline class="banner_background_video"
+            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; z-index: 1;">
+            <source src="<?php echo get_template_directory_uri(); ?>/images/banner-video.mp4" type="video/mp4">
+        </video>
+    </div>
 
+    <div class="banner-main-slider-item"
+        style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/stack_01.png');">
+        >
+        <div class="container">
+            <div class="banner_content text_center">
+                <h2 class="f_80 color_white" data-aos="fade-up" data-aos-duration="1200" data-aos-once="true">
+                    Commercial Space</h2>
+                <p class="f_32 mt_20 color_white" data-aos="fade-up" data-aos-duration="1200" data-aos-once="true">
+                    Whether you're designing a workspace, a cultural venue, or a healthcare
+                    facility, our experts are here to help you achieve the perfect acoustic solution.</p>
+                <div data-aos="fade-up" class="mt_40" data-aos-duration="1200" data-aos-once="true">
+                    <a href="/services" class="btn_style">Discover Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="banner-main-slider-item"
+        style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/stack_02.png');">
+        >
+        <div class="container">
+            <div class="banner_content text_center">
+                <h2 class="f_80 color_white" data-aos="fade-up" data-aos-duration="1200" data-aos-once="true">
+                    The Symfony of products</h2>
+                <p class="f_32 mt_20 color_white" data-aos="fade-up" data-aos-duration="1200" data-aos-once="true">
+                    Advanced acoustic ceiling solutions</p>
+                <div data-aos="fade-up" data-aos-duration="1200" data-aos-once="true" class="mt_40">
+                    <a href="/services" class="btn_style">Discover Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
-<video autoplay loop muted playsinline class="banner_background_video" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; z-index: 1;">
-                    <source src="<?php echo get_template_directory_uri(); ?>/images/banner-video.mp4" type="video/mp4">
-                </video>
-</div>
-<div class="banner-main-slider-item" style="background:blue;">d</div>
-<div class="banner-main-slider-item" style="background:green;">d</div>
-
-<!-- Pagination dots -->
-<div class="banner-slider-dots show"></div>
+    <!-- Pagination dots -->
+    <div class="banner-slider-dots show"></div>
 </section>
